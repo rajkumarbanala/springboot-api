c4d89e84467f3921052c52a7d1e1d2e8a6ddb2ab
Rajkumar Banala <rajkumar@icorei.com>
2022-05-09 13:07:51 +0530

    Added Lombok, Swagger Documentation

--- src/main/java/com/example/demo/dto/StudentCreateResponseDTO.java
+++ src/main/java/com/example/demo/dto/StudentCreateResponseDTO.java
@@ -4,63 +4,34 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class StudentCreateResponseDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
 	@JsonProperty("id")
 	private int id;
+	
 	@JsonProperty("firstName")
 	private String firstName;
+	
 	@JsonProperty("lastName")
 	private String lastName;
+	
 	@JsonProperty("userName")
 	private String userName;
+	
 	@JsonProperty("password")
 	private String password;
+	
 	@JsonProperty("email")
 	private String email;
+	
 	@JsonProperty("mobile")
 	private double mobile;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public double getMobile() {
-		return mobile;
-	}
-	public void setMobile(double mobile) {
-		this.mobile = mobile;
-	}
-	
 	
 }
--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -9,6 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 @Entity
 @Table(name = "student")
 public class Student implements Serializable {
@@ -37,61 +42,5 @@ public class Student implements Serializable {
 
 	@Column(name = "mobile")
 	private double mobile;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public double getMobile() {
-		return mobile;
-	}
-
-	public void setMobile(double mobile) {
-		this.mobile = mobile;
-	}
-
+	
 }
