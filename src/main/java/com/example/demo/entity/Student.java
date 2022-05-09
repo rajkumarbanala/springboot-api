8f613d961e28180da1cf6ecd671bd4fbdff1422c
Rajkumar Banala <rajkumar@icorei.com>
2022-05-09 12:22:27 +0530

    Added Loggers, Code Cleanup

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -10,67 +10,88 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="student")
-
+@Table(name = "student")
 public class Student implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private int id;
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String firstName;
-	@Column(name="last_name")
+
+	@Column(name = "last_name")
 	private String lastName;
-	@Column(name="user_name")
+
+	@Column(name = "username")
 	private String userName;
-	@Column(name="password")
+
+	@Column(name = "password")
 	private String password;
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	@Column(name="mobile")
+
+	@Column(name = "mobile")
 	private double mobile;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public double getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(double mobile) {
 		this.mobile = mobile;
 	}
 
-	
 }
--- src/main/java/com/example/demo/service/StudentServce.java
+++ src/main/java/com/example/demo/service/StudentServce.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import com.example.demo.dto.StudentCreateRequestDTO;
 import com.example.demo.dto.StudentCreateResponseDTO;
-import com.example.demo.entity.Student;
 
-public interface StudentServceI {
+public interface StudentServce {
 
 	public List<StudentCreateResponseDTO> getAllStudents();
+
 	public StudentCreateResponseDTO addStudent(StudentCreateRequestDTO studentCreateRequestDTO);
+
 	public StudentCreateResponseDTO getStudent(int id);
+
 	public StudentCreateResponseDTO updateStudent(StudentCreateRequestDTO studentCreateRequestDTO);
-	public void deleteStudent(int id);
-		
+
+	public boolean deleteStudent(int id);
+
 }
